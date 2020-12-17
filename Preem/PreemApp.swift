f0daf518be43f90628c400c0c232655c39e67b87
tpubz <taylor.pubins@icloud.com>
2020-12-17 12:29:03 -0700

    Initial Commit

---
+++ Preem/PreemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreemApp.swift
+//  Preem
+//
+//  Created by Taylor Pubins on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct PreemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
