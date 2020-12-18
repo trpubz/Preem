f4cbe11077e4cc586bb4c99ac6cd2c4d5d087203
trpubz <taylor.pubins@me.com>
2020-12-18 07:43:41 -0700

    onboarding model

---
+++ Preem/Models/Asset Models/Onboarding.swift
@@ -0,0 +1,22 @@
+//
+//  Onboarding.swift
+//  Preem
+//
+//  Created by Taylor Pubins on 12/17/20.
+//
+
+import Foundation
+import SwiftUI
+
+struct Onboarding {
+    let icon: [Image] = [
+        Image("income"),
+        Image("investment"),
+        Image("incomeplant")
+    ]
+    
+    let text: [String] = [
+        "track trade performance against market indicies",
+        "d"
+    ]
+}
